5a1d7bbeb7754a76a08f74bee5772f265b6a96f0
Mikolaj Lajming <mikolaj.lajming@gmail.com>
2022-11-10 14:17:18 +0100

    git ignore

--- Manim/combine_sinewaves_animated.py
+++ Manim/combine_sinewaves_animated.py
@@ -1,247 +1,69 @@
 from manim import *
-import numpy as np
+from random import choice, uniform
 
-number_of_terms = 6
-
-
-class TrigScene(Scene):
-    def __init__(self,
-                 x_min=-0.5,
-                 x_max=np.pi * 2 + 0.5,
-                 y_min=-1.5,
-                 y_max=1.5,
-                 x_ticks=[np.pi * 0.5, np.pi, 3.0 / 2.0 * np.pi, 2 * np.pi],
-                 x_tick_labels=[MathTex("{1 \over 2} \pi"),
-                                MathTex("\pi"), MathTex("{3 \over 2} \pi"), MathTex("2 \pi")],
-                 y_ticks=[-1, 1],
-                 y_tick_labels=[
-                     MathTex("-1"), MathTex("1")],
-                 x_axis_scale=1.5,
-                 y_axis_scale=2,
-                 axis_color=GRAY,
-                 tick_labels_color=WHITE,
-                 ** kwargs):
-        super().__init__(**kwargs)
-        self.x_min = x_min
-        self.x_max = x_max
-        self.y_min = y_min
-        self.y_max = y_max
-        self.axis_color = axis_color
-        self.tick_labels_color = tick_labels_color
-        self.x_ticks = x_ticks
-        self.x_tick_labels = x_tick_labels
-        self.y_ticks = y_ticks
-        self.y_tick_labels = y_tick_labels
-        self.scale = 1
-        self.x_axis_scale = x_axis_scale
-        self.y_axis_scale = y_axis_scale
-        self.origin_point = np.array(
-            [-(self.x_max-self.x_min) / 2.0 * self.x_axis_scale, -0.5, 0])
-        self.graph = VGroup()
-
-    def show_axis(self, animate=False):
-        x_start = np.array([self.x_min * self.x_axis_scale, 0, 0])
-        x_end = np.array([self.x_max * self.x_axis_scale, 0, 0])
-
-        y_start = np.array([0, self.y_min * self.y_axis_scale, 0])
-        y_end = np.array([0, self.y_max * self.y_axis_scale, 0])
-
-        x_axis = Line(x_start, x_end)
-        y_axis = Line(y_start, y_end)
-        x_axis.set_color(self.axis_color)
-        y_axis.set_color(self.axis_color)
-
-        #x_axis.shift(self.origin_point * 0.5)
-        x_axis.move_to(self.origin_point + x_axis.get_center())
-        y_axis.move_to(self.origin_point + y_axis.get_center())
-
-        #self.add(x_axis, y_axis)
-        self.graph.add(x_axis, y_axis)
-        self.add_x_ticks()
-        # self.add_x_labels()
-        self.add_y_ticks()
-        # self.add_y_labels()
-        self.graph.scale(self.scale)
-        if animate:
-            self.play(Write(self.graph))
-        else:
-            self.add(self.graph)
-
-    def add_x_ticks(self):
-        height = 0.15
-        line = Line(np.array([0, -height, 0]), np.array([0, height, 0]))
-        line.set_color(self.axis_color)
-
-        for i in range(len(self.x_ticks)):
-            copy = line.copy()
-            copy.move_to(self.origin_point +
-                         np.array([self.x_ticks[i] * self.x_axis_scale, 0, 0]))
-            # self.add(copy)
-            self.graph.add(copy)
-
-    def add_y_ticks(self):
-        width = 0.15
-        line = Line(np.array([-width, 0, 0]), np.array([width, 0, 0]))
-        line.set_color(self.axis_color)
-
-        for i in range(len(self.y_ticks)):
-            copy = line.copy()
-            copy.move_to(self.origin_point +
-                         np.array([0, self.y_ticks[i] * self.y_axis_scale, 0]))
-            # self.add(copy)
-            self.graph.add(copy)
-
-    def add_x_labels(self):
-        for i in range(len(self.x_tick_labels)):
-            self.x_tick_labels[i].next_to(
-                self.origin_point + np.array([self.x_ticks[i] * self.x_axis_scale, 0, 0]), DOWN)
-            self.x_tick_labels[i].set_color(self.tick_labels_color)
-            self.graph.add(self.x_tick_labels[i])
-
-    def add_y_labels(self):
-        for i in range(len(self.y_tick_labels)):
-            self.y_tick_labels[i].next_to(
-                self.origin_point + np.array([0, self.y_ticks[i] * self.y_axis_scale, 0]), LEFT)
-            self.y_tick_labels[i].set_color(self.tick_labels_color)
-            self.graph.add(self.y_tick_labels[i])
-
-    def get_graph(self, func, color=None, x_min=None, x_max=None, **kwargs):
-        if color is None:
-            color = BLUE
-        if x_min is None:
-            x_min = self.x_min
-        if x_max is None:
-            x_max = self.x_max
-
-        def parameterized_function(alpha):
-            x = interpolate(x_min, x_max, alpha)
-            y = func(x)
-            if not np.isfinite(y):
-                y = self.y_max
-            # return self.coords_to_point(x, y)
-            return np.array([x * self.x_axis_scale, y * self.y_axis_scale, 0]) + self.origin_point
-
-        graph = ParametricFunction(
-            parameterized_function, color=color, **kwargs)
-        graph.underlying_function = func
-        # graph.move_to(self.origin_point)
-        graph.scale(self.scale)
-        return graph
-
-    def coords_to_point(self, x, y):
-        result = self.x_axis.number_to_point(x)[0] * RIGHT
-        result += self.y_axis.number_to_point(y)[1] * UP
-        return result
+ITERATIONS = 12
 
 
-class TrigSumScene(TrigScene):
-    def __init__(self, number_of_terms=5, text_scale=0.7, wait_time=0.25, runtime=0.5, **kwargs):
-        super().__init__(**kwargs)
-        self.index = 1
-        self.text_scale = text_scale
-        self.number_of_terms = number_of_terms
-        self.term_color = RED
-        self.sum_color = BLUE
-        self.wait_time = wait_time
-        self.runtime = runtime
+class TestSine(Scene):
+
+    def __init__(self):
+        super(TestSine, self).__init__()
+        self.amplitudes = []
+        self.frequencies = []
+        self.phases = []
+        self.colors = [WHITE, BLUE_A, BLUE_B, BLUE_C, BLUE_D, BLUE_E, PURE_BLUE, TEAL_A, TEAL_B,
+                       TEAL_C, TEAL_D, TEAL_E, GREEN_A, GREEN_B, GREEN_C, GREEN_D, GREEN_E, PURE_GREEN,
+                       YELLOW_A, YELLOW_B, YELLOW_D, YELLOW_E, GOLD_A, GOLD_B, GOLD_C, GOLD_D, GOLD_E,
+                       RED_A, RED_B, RED_C, RED_D, RED_E, PURE_RED, MAROON_A, MAROON_B, MAROON_C, MAROON_D,
+                       MAROON_E, PURPLE_A, PURPLE_B, PURPLE_C, PURPLE_D, PURPLE_E, PINK, LIGHT_PINK,
+                       ORANGE, LIGHT_BROWN]
 
     def construct(self):
-        self.show_axis(animate=True)
-        # text = self.get_tex()
-        # self.play(Write(text))
 
-        sumGraph = self.get_graph(self.get_func())
-        sumGraph.set_color(self.sum_color)
-        self.play(Create(sumGraph), runtime=self.runtime)
+        ax = Axes(y_range=(-2, 2), x_range=(0, 20), y_length=4, x_length=10)
+        group = VGroup()
 
-        for j in range(2, self.number_of_terms):
-            self.next()
-            termGraph = self.get_graph(self.get_func())
-            termGraph.set_color(self.term_color)
-            self.play(Create(termGraph), runtime=self.runtime)
-            self.wait(self.wait_time)
+        for i in range(0, ITERATIONS):
+            # harmonic_amplitude = round((uniform(1, 2) - (i / 20)), 2)
+            harmonic_amplitude = round((2 - (i / 12)), 2)
+            self.amplitudes.append(harmonic_amplitude)
+            harmonic_frequency = i + 1
+            self.frequencies.append(harmonic_frequency)
+            # harmonic_phase = uniform(0.0, 1.0)
+            harmonic_phase = 0
+            self.phases.append(harmonic_phase)
+            harmonic_curve = ax.plot(
+                self.generate_curve(harmonic_amplitude, harmonic_frequency, harmonic_phase)
+            )
+            if i != 0:
+                harmonic_curve.color = choice(self.colors)
+                harmonic_curve.stroke_width = 0.5
+            else:
+                harmonic_curve.color = BLUE
+                harmonic_curve.stroke_width = 0.8
+            group.add(harmonic_curve)
+        curve_final = ax.plot(lambda x: self.combine_curves(x))
+        curve_final.set_color(YELLOW)
+        # self.add(curve_final)
+        self.play(Create(ax))
+        # self.play(Create(group[0]), Create(group[1]), Create(group[2]), Create(group[3]), Create(group[4]), Create(group[5]), Create(group[6]), Create(group[7]), Create(group[8]), Create(group[9]), Create(group[10]), Create(group[11]), run_time=3)
+        self.play(Create(group), run_time=5)
+        self.play(ReplacementTransform(group, curve_final), run_time=2)
+        self.play(FadeOut(ax))
 
-            nextSumGraph = self.get_graph(self.get_sum_func())
-            nextSumGraph.set_color(self.sum_color)
-            # nextText = self.get_tex()
+    def generate_curve(self, ampl, freq, ph):
+        return lambda x: self.math_function(ampl=ampl, freq=freq, ph=ph, x=x)
 
-            self.play(
-                ReplacementTransform(
-                    VGroup(sumGraph, termGraph), nextSumGraph),
-                # ReplacementTransform(text, nextText),
-                runtime=self.runtime)
-            self.wait(self.wait_time)
+    def math_function(self, ampl, freq, ph, x):
+        return ampl * (1.0 / freq) * np.sin(freq * (x - ph))
 
-            sumGraph = nextSumGraph
-            # text = nextText
+    def combine_curves(self, x):
+        cc = 0
+        for n in range(0, ITERATIONS):
+            cc += self.amplitudes[n] * (1.0 / self.frequencies[n]) * np.sin(self.frequencies[n] * (x - self.phases[n]))
+        return cc
 
-    def show_initial_graph(self):
-        graph1 = self.get_graph(self.get_func())
-        self.play(Create(graph1))
-        self.wait(0.5)
-
-    def get_tex_at(self, index):
-        return MathTex(r"f_{"+str(index)+"}(x) = {2 \over \pi} \sum^{"+str(index)+"}_{k=1}{(-1)^{(k-1)} \over k} \sin(k \cdot x)")
-
-    def get_tex(self):
-        text = self.get_tex_at(self.index)
-        text.to_corner(UP)
-        text.scale(self.text_scale)
-        text.set_color(self.sum_color)
-        return text
-
-    def get_func(self):
-        return self.get_func_at(self.index)
-
-    def get_func_at(self, index):
-        return lambda x: (2.0 / np.pi) * (1.0 / index) * np.sin(index * x)
-
-    def get_sum_func_at(self, index):
-        def sum_func(x):
-            s = 0
-            for j in range(1, index+1):
-                s = s + self.get_func_at(j)(x)
-            return s
-        return sum_func
-
-    def get_sum_func(self):
-        return self.get_sum_func_at(self.index)
-
-    def next(self):
-        self.index = self.index + 1
-
-
-class Saw(TrigSumScene):
-    def __init__(self, **kwargs):
-        super().__init__(number_of_terms=number_of_terms, **kwargs)
-
-    def construct(self):
-        super().construct()
-
-
-class Pulse(TrigSumScene):
-    def __init__(self, **kwargs):
-        super().__init__(number_of_terms=number_of_terms, **kwargs)
-
-    def construct(self):
-        super().construct()
-
-    def get_func_at(self, index):
-        return lambda x: (4.0 / np.pi) * (1.0 / (2*index - 1)) * np.sin((2*index-1) * x)
-
-    def get_tex_at(self, index):
-        return MathTex(r"f_{"+str(index)+"}(x) = {4 \over \pi} \sum^{"+str(index)+"}_{k=1}{1 \over (2k - 1)} \sin((2k - 1) \cdot x)")
-
-
-class Tri(TrigSumScene):
-    def __init__(self, **kwargs):
-        super().__init__(number_of_terms=number_of_terms, **kwargs)
-
-    def construct(self):
-        super().construct()
-
-    def get_func_at(self, index):
-        return lambda x: 8.0 / (np.pi**2) * (((-1.0)**(index-1)) / (2*index - 1)**2) * np.sin((2*index-1) * x)
-
-    def get_tex_at(self, index):
-        return MathTex(r"f_{"+str(index)+"}(x) = {8 \over \pi^2} \sum^{"+str(index)+"}_{k=1}{(-1)^{k-1} \over (2k - 1)^2} \sin((2k - 1) \cdot x)")
+# class testSquare(Scene):
+#     def construct(self):
+#         square = Square(color=RED, fill_opacity=0.5)
+#         self.play(DrawBorderThenFill(square, run_time=4))
