74ce6aae47003011261648c5b06706c146c61da2
MikolajLajming <mikolaj.lajming@gmail.com>
2022-11-14 16:13:56 +0100

    Pong - basic working version

--- Intermediate/Game004_pong/main.py
+++ Intermediate/Game004_pong/main.py
@@ -24,40 +24,63 @@ screen.update()
 ball.initial_move(True)
 screen.update()
 
+game = True
 game_on = True
-while game_on:
-    screen.update()
-    ball_x_cor = ball.xcor()
-    ball_y_cor = ball.ycor()
-    r_paddle_y_cor = r_paddle.ycor()
-    l_paddle_y_cor = l_paddle.ycor()
-    n = 1
-    if ball_x_cor >= 330 or ball_x_cor <= -330:
-        right_paddle = True if ball_x_cor > 0 else False
-        if (r_paddle_y_cor - 60) < ball_y_cor < (r_paddle_y_cor + 60) and right_paddle \
-                or (l_paddle_y_cor - 60) < ball_y_cor < (l_paddle_y_cor + 60) and not right_paddle:
-            paddle_y_cor = r_paddle_y_cor if right_paddle else l_paddle_y_cor
-            if ball_y_cor > paddle_y_cor:
-                ball.angle = (dist([paddle_y_cor], [ball.ycor()])) / 60
-                print(ball.angle)
-                ball.move_ball(True, True)
-            elif ball_y_cor <= paddle_y_cor:
-                ball.angle = (dist([paddle_y_cor], [ball.ycor()])) / 60
-                print(ball.angle)
-                ball.move_ball(True, False)
-        else:
-            if right_paddle:
-                scoreboard.P2_score += 1
-                ball.reset_ball(not right_paddle)
-                screen.update()
-                time.sleep(1)
+
+# def pause_game():
+#     global game_on
+#     if game_on:
+#         game_on = False
+#         scoreboard.pause()
+#     else:
+#         game_on = True
+#         scoreboard.update_score()
+#
+# def quit_game():
+#     global game
+#     game = False
+#
+#
+# screen.onkey(pause_game, "p")
+# screen.onkey(quit_game, "q")
+
+while game:
+    while game_on:
+        screen.update()
+        ball_x_cor = ball.xcor()
+        ball_y_cor = ball.ycor()
+        r_paddle_y_cor = r_paddle.ycor()
+        l_paddle_y_cor = l_paddle.ycor()
+        n = 1
+        if ball_x_cor >= 330 or ball_x_cor <= -330:
+            right_paddle = True if ball_x_cor > 0 else False
+            if (r_paddle_y_cor - 60) < ball_y_cor < (r_paddle_y_cor + 60) and right_paddle \
+                    or (l_paddle_y_cor - 60) < ball_y_cor < (l_paddle_y_cor + 60) and not right_paddle:
+                paddle_y_cor = r_paddle_y_cor if right_paddle else l_paddle_y_cor
+                if ball_y_cor > paddle_y_cor:
+                    ball.angle = (dist([paddle_y_cor], [ball.ycor()])) / 60
+                    ball.move_ball(True, True)
+                elif ball_y_cor <= paddle_y_cor:
+                    ball.angle = (dist([paddle_y_cor], [ball.ycor()])) / 60
+                    ball.move_ball(True, False)
             else:
-                scoreboard.P1_score += 1
-                ball.reset_ball(not right_paddle)
-                screen.update()
-                time.sleep(1)
-    else:
-        ball.move_ball(False, False)
-    time.sleep(0.01)
+                if right_paddle:
+                    scoreboard.increase_score(False)
+                    ball.reset_ball(not right_paddle)
+                    screen.update()
+                    time.sleep(1)
+                else:
+                    scoreboard.increase_score(True)
+                    ball.reset_ball(not right_paddle)
+                    screen.update()
+                    time.sleep(1)
+        else:
+            ball.move_ball(False, False)
+        time.sleep(0.01)
+        if scoreboard.P2_score >= 10 or scoreboard.P1_score >= 10:
+            game = False
+            game_on = False
+            scoreboard.game_over()
+    time.sleep(2)
 
 screen.exitonclick()
--- Intermediate/Game004_pong/scoreboard.py
+++ Intermediate/Game004_pong/scoreboard.py
@@ -7,3 +7,28 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.P1_score = 0
         self.P2_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 255)
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(0, 255)
+        self.write(f"{self.P2_score} : {self.P1_score}", align="center", font=("Courier", 24, "normal"))
+
+    def increase_score(self, p1=bool):
+        if p1:
+            self.P1_score += 1
+        else:
+            self.P2_score += 1
+        self.clear()
+        self.update_score()
+
+    # def pause(self):
+    #     self.goto(0, 0)
+    #     self.write("PAUSE\nPress P to resume, Q to quit", align="center", font=("Courier", 20, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=("Courier", 24, "normal"))
